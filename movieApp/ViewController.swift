417dfbf490db3d1e54a66b8ffa7389448230839f
keremec <k.safa.dirican@outlook.com>
2022-11-05 09:08:16 +0300

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Kerem Safa Dirican on 5.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
