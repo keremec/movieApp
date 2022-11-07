23f43834e07dc6d066f5773334c06597f9d13def
keremec <k.safa.dirican@outlook.com>
2022-11-07 16:20:49 +0300

    Data fetching functions completed

--- movieApp/homepage-module/HomepageInteractor.swift
+++ movieApp/homepage-module/HomepageInteractor.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class HomepageInteractor:PtoI_HomepageProtocol{
     
@@ -15,16 +16,55 @@ class HomepageInteractor:PtoI_HomepageProtocol{
         
         var movieList = [Movies]()
         
-        let cell1 = Movies(id: 23, imgURL: "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_SX300.jpg", title: "Blade Runner 2049", specs: "2017 | Action, Adventure | 2h43m", plot: "DENEME", rating_user: " 3.3", rating_critic: " 33", esrb: "PG-14")
+        //MARK: Searching Movies
         
-        movieList.append(cell1)
-        movieList.append(cell1)
-        movieList.append(cell1)
-        movieList.append(cell1)
-        movieList.append(cell1)
-        movieList.append(cell1)
-        
-        self.homepagePresenter?.movieSendtoPresenter(movieList: movieList)
+        AF.request("https://www.omdbapi.com/?s=deadpool+2&type=movie&apikey=26f7e70a",method: .get).response { response in
+            if let data = response.data {
+                do{
+                    let result = try JSONDecoder().decode(MovieSearchResponse.self, from: data)
+                    for i in result.search{
+                        //MARK: - Adding Movies
+                        AF.request("https://www.omdbapi.com/?i=\(i.imdbID)&apikey=26f7e70a",method: .get).response { response in
+                            if let data = response.data {
+                                do{
+                                    let result = try JSONDecoder().decode(MovieDetailResponse.self, from: data)
+                                    let movie = Movies(id: Int(result.imdbID.dropFirst(2))!, imgURL: result.poster, title: result.title, specs: self.subLabelGenerator(year: result.year, genre: result.genre, runtime: result.runtime), plot: result.plot, rating_user: result.imdbRating, rating_critic: result.metascore, esrb: result.rated)
+                                    movieList.append(movie)
+                                    self.homepagePresenter?.movieSendtoPresenter(movieList: movieList)
+                                    
+                                }catch{
+                                    print(error.localizedDescription)
+                                }
+                            }
+                        }
+                        //MARK: -
+                    }
+                    
+                }catch{
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
     
+//Helper Functions
+    
+    func subLabelGenerator(year:String, genre:String, runtime:String) -> String {
+        var duration = Int(runtime.dropLast(4)) ?? 0
+        print(duration)
+        let hours = duration / 60
+        let minutes = duration % 60
+        if hours > 0{
+            return "\(year) | \(genre) | \(hours)h\(minutes)m"
+        }
+        else if minutes > 0{
+            return "\(year) | \(genre) | \(minutes)m"
+        }
+        else{
+            return "\(year) | \(genre)"
+        }
+    }
+    
+    
 }
+
